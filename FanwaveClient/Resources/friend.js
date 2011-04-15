9501006cad4b519808782330dbcf978e9a08a29c
Ken Sun <ken.sun@wildmindcorp.com>
2011-04-15 18:34:39 +0800

    WebSocket fixed

--- FanwaveClient/Resources/friend.js
+++ FanwaveClient/Resources/friend.js
@@ -3,3 +3,13 @@ var win = Titanium.UI.currentWindow;
 // input variables
 //
 var currentUser = win.currentUser;
+
+var web = Ti.UI.createWebView({
+	url: 'http://www.fanwave.tv/JSMVC/beagle/beagle.html'
+});
+win.add(web);
+
+web.addEventListener('load', function () {
+	var action = '$(\"#userName\").val(\"ken.sun@wildmindcorp.com\");$(\"#password\").val(\"wewe\");$(\"#logInButton\").click();';
+	web.evalJS(action);
+});
